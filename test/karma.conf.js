03a70d9e8c2ed85de614ee28f6658c56686d8d55
Kipp Ashford <volkipp@gmail.com>
2014-10-08 20:55:49 -0700

    Compiled new bin

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
         // - PhantomJS
         // - IE (only Windows)
         // CLI --browsers Chrome,Firefox,Safari
-        browsers : ['Firefox'],
+        browsers : ['Chrome'],
 
         // If browser does not capture in given timeout [ms], kill it
         // CLI --capture-timeout 60000
